36326c41dd31bca2909480ef17b10888dad004b2
royel21 <royelconsoro@hotmail.com>
2019-04-01 06:26:13 -0400

    litte experiment

--- public/js/player.js
+++ public/js/player.js
@@ -8,11 +8,17 @@ const nextAudio = (e) => {
 }
 
 const prevAudio = (e) => {
-    let index = getIndex(config.currentFile);
-    if (--index > -1 && index < playList.length) {
-        playAudio(playList[index]);
-    } else {
-        playAudio(playList[playList.length - 1]);
+    // let index = getIndex(config.currentFile);
+    // if (--index > -1 && index < playList.length) {
+    //     playAudio(playList[index]);
+    // } else {
+    //     playAudio(playList[playList.length - 1]);
+    // }
+    var turnOffDisplay = require('turn-off-display');
+    try {
+        turnOffDisplay();
+    } catch (err) {
+        // handle error
     }
 }
 
